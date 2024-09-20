28d1c8bbccf4ee04185b23beba263b0bc43a727b
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2024-09-20 13:50:53 +0200

    forms

--- src/Repository/MicroPostRepository.php
+++ src/Repository/MicroPostRepository.php
@@ -8,6 +8,11 @@ use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<MicroPost>
+ *
+ * @method MicroPost|null find($id, $lockMode = null, $lockVersion = null)
+ * @method MicroPost|null findOneBy(array $criteria, array $orderBy = null)
+ * @method MicroPost[]    findAll()
+ * @method MicroPost[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class MicroPostRepository extends ServiceEntityRepository
 {
@@ -16,28 +21,21 @@ class MicroPostRepository extends ServiceEntityRepository
         parent::__construct($registry, MicroPost::class);
     }
 
-    //    /**
-    //     * @return MicroPost[] Returns an array of MicroPost objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('m.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function add(MicroPost $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
 
-    //    public function findOneBySomeField($value): ?MicroPost
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(MicroPost $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
 }
